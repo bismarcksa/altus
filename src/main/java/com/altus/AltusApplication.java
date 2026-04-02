23787ac1775e64d6b427051d26af2d7bab0f70da
Bismarck Almeida <bismarck.almeida@hotmail.com>
2026-04-02 07:12:21 -0300

    Commit de sexta-feira

---
+++ src/main/java/com/altus/AltusApplication.java
@@ -0,0 +1,11 @@
+package com.altus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AltusApplication.class, args);
+    }
+}
---
+++ src/main/java/com/altus/model/Funcionario.java
@@ -0,0 +1,108 @@
+package com.altus.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import org.hibernate.validator.constraints.br.CPF;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Embedded;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.SequenceGenerator;
+//import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+//@Entity
+//@Table(name = "funcionario")
+public class Funcionario implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+//    @Id 
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_funcionario")
+//	@SequenceGenerator(name =  "sequence_funcionario", sequenceName = "sequence_funcionario", initialValue = 1, allocationSize = 1)
+    private Long codigo;
+
+    @NotBlank(message = "Nome é obrigatório.")
+    private String nome;
+
+    @NotBlank(message = "CPF é obrigatório.")
+//    @CPF(message = "CPF inválido")
+//    @Column(name = "cpf")
+    private String cpf;
+
+    private String telefone;
+
+    @Email(message = "E-mail inválido")
+    private String email;
+
+//    @JsonIgnore
+//    @Embedded
+ //   private Endereco endereco;
+      
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+/*    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario funcionario = (Funcionario) o;
+        return Objects.equals(codigo, funcionario.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
